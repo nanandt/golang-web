fe34c8c9d72dfa5ecb9c9cd1819ae22ecf6ab9fb
nanandt <rizkyfatih007@gmail.com>
2022-04-14 23:10:50 +0700

    Form Post

---
+++ form_post_test.go
@@ -0,0 +1,37 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func FormPost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	firstName := r.PostForm.Get("firsName")
+	lastName := r.PostForm.Get("lastName")
+
+	fmt.Fprintf(w, "Hello %s %s", firstName, lastName)
+}
+
+func TestFormPost(t *testing.T) {
+	requestBody := strings.NewReader("firstName=Muhammad&lastName=Rizky")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
