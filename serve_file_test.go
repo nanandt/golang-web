5438efc288cb754fbe7430c2bf502fbd0d9f665e
nanandt <rizkyfatih007@gmail.com>
2022-04-16 06:20:42 +0700

    Serve File

---
+++ serve_file_test.go
@@ -0,0 +1,54 @@
+package golang_web
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func ServeFile(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		http.ServeFile(w, r, "./resources/ok.html")
+	} else {
+		http.ServeFile(w, r, "./resources/notfound.html")
+	}
+}
+
+func TestServeFileServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/ok.html
+var resourcesOk string
+
+//go:embed resources/notfound.html
+var resourcesNotFound string
+
+func ServeFileEmbed(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		fmt.Fprint(w, resourcesOk)
+	} else {
+		fmt.Fprint(w, resourcesNotFound)
+	}
+}
+
+func TestServeFileServerEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
