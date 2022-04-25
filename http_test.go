4393ad310fcc82d8dd2d43c6a5f496d4a152b1e1
nanandt <rizkyfatih007@gmail.com>
2022-04-25 17:21:27 +0700

    HTTP Test

--- http_test.go
+++ http_test.go
@@ -1,1 +1,24 @@
 package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func TestHttp(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+	response := recorder.Result()
+	all, _ := io.ReadAll(response.Body)
+	bodyString := string(all)
+	fmt.Println(bodyString)
+}
