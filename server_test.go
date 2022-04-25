36841d76fd4ba2fd69eaf9ac3012bd65d339d4d6
nanandt <rizkyfatih007@gmail.com>
2022-04-25 17:20:33 +0700

    Server

--- server_test.go
+++ server_test.go
@@ -1,1 +1,19 @@
 package golang_web
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	// bikin server
+	server := http.Server{
+		Addr: "localhost:8080",
+	}
+
+	// menjalankan server
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
